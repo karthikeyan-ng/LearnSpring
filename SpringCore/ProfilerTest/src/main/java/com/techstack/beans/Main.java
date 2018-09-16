78864745634470670e9f8f9a9f5a47a3ab064b40
Karthikeyan Nithiyanandam <39563373+karthikeyan-ng@users.noreply.github.com>
2018-09-16 12:16:52 +0530

    Created Profiler Test application where we can use agent application.
    Created Profiler Test application to test activated profiler for debug with method execution timer.

---
+++ SpringCore/ProfilerTest/src/main/java/com/techstack/beans/Main.java
@@ -0,0 +1,32 @@
+package com.techstack.beans;
+
+/**
+ * 
+ * @author Karthikeyan N
+ *
+ */
+public class Main {
+	
+	/**
+	 * server side: domain startup script
+	 * 
+	 * set JAVA_OPTIONS=-javaagent:c:/domains/profiler.jar
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		
+		Sub1 sub1 = new Sub1();
+		sub1.a();
+		sub1.b();
+		sub1.c();
+
+		Sub2 sub2 = new Sub2();
+		sub2.d();
+		sub2.e();
+		sub2.f();
+		
+	}
+	
+}
